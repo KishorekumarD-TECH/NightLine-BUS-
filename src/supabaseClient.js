5d71890f862d63304f760d457c0baa488557c7c8
KishorekumarD-TECH <kishorekumard.tech@gmail.com>
2026-08-30 17:55:09 +0530

    fix supabase config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-
-const url = import.meta.env.VITE_SUPABASE_URL
-const anon = import.meta.env.VITE_SUPABASE_ANON_KEY
-
+﻿import { createClient } from '@supabase/supabase-js'
+const url = import.meta.env.VITE_SUPABASE_URL || 'https://lvajupbineiwdgdtnkpq.supabase.co'
+const anon = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_WOeZ8NYm-uMjmDoQc9Y1qQ_k7qbNofe'
 export const supabase = createClient(url, anon)
